948710549b8f90ec3529691eb3f01c0460bb9c08
heet-1011 <heetp1011@gmail.com>
2021-04-27 18:13:39 +0530

    UserInfo & DoctorInfo Complete Modification

--- app/src/main/java/com/hp/appoindone/splash_screen.java
+++ app/src/main/java/com/hp/appoindone/splash_screen.java
@@ -150,7 +150,6 @@ public class splash_screen extends AppCompatActivity {
     public void nxtscreen(){
         Intent intent = new Intent(splash_screen.this,MainActivity.class);
         intent.putExtra("pincodepass",pincode);
-        finish();
         startActivity(intent);
     }
 
--- app/src/main/java/com/hp/appoindone/userinfo.java
+++ app/src/main/java/com/hp/appoindone/userinfo.java
@@ -3,13 +3,60 @@ package com.hp.appoindone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CompoundButton;
+import android.widget.PopupMenu;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 public class userinfo extends AppCompatActivity {
-
+    RadioGroup radioGroup;
+    RadioButton rbmale,rbfemale,rbother;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userinfo);
+        initviews();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rb_ui_male :  rbmale.setTextColor(Color.parseColor("#303030"));
+                                            rbfemale.setTextColor(Color.parseColor("#50303030"));
+                                            rbother.setTextColor(Color.parseColor("#50303030"));
+                                            break;
+                    case R.id.rb_ui_female : rbmale.setTextColor(Color.parseColor("#50303030"));
+                                             rbfemale.setTextColor(Color.parseColor("#303030"));
+                                             rbother.setTextColor(Color.parseColor("#50303030"));
+                                             break;
+                    case R.id.rb_ui_other : rbmale.setTextColor(Color.parseColor("#50303030"));
+                                            rbfemale.setTextColor(Color.parseColor("#50303030"));
+                                            rbother.setTextColor(Color.parseColor("#303030"));
+                                            break;
+                }
+            }
+        });
+    }
+
+    private void initviews() {
+        radioGroup = findViewById(R.id.radiogroup);
+        rbmale = findViewById(R.id.rb_ui_male);
+        rbfemale = findViewById(R.id.rb_ui_female);
+        rbother = findViewById(R.id.rb_ui_other);
     }
 }
